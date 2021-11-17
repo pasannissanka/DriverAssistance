27d74d02ab18c4752612016a16e0bd52beae8d31
pasannissanka <pasannissanka@hotmail.com>
2021-11-18 02:03:29 +0530

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Smart Driver Assistance System
+
+### forked from [SunnyGrocery/ncnn-android-yolov4-tiny-custom](https://github.com/SunnyGrocery/ncnn-android-yolov4-tiny-custom)  
+
+> Model trained by AlexeyAB/darknet,which implement the YOLOv4-tiny algorithm,
+> Model converted and deployed by Tencent/NCNN.
+
+
+### Build
+
++ Using [Android Studio](https://developer.android.com/studio) to build this project.
++ You need install Android SDK , Android NDK and CMake additional in Android Studio.
+
+
+---
+
+### Reference：
+
++ [AlexeyAB/darknet](https://github.com/AlexeyAB/darknet)
++ [dog-qiuqiu/YOLOv5_NCNN](https://github.com/dog-qiuqiu/YOLOv5_NCNN)
++ [nihui/ncnn-android-yolov5](https://github.com/nihui/ncnn-android-yolov5)
++ [SunnyGrocery/ncnn-android-yolov4-tiny-custom](https://github.com/SunnyGrocery/ncnn-android-yolov4-tiny-custom)
---
+++ app/src/main/cpp/yolov4.h
@@ -0,0 +1,69 @@
+#ifndef YOLOV4_H
+#define YOLOV4_H
+
+#include "ncnn/net.h"
+
+namespace cv {
+    typedef struct {
+        int width;
+        int height;
+    } Size;
+}
+
+typedef struct {
+    std::string name;
+    int stride;
+    std::vector<cv::Size> anchors;
+} YoloLayerData;
+
+typedef struct BoxInfo {
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float score;
+    int label;
+
+} BoxInfo;
+
+class yolov4 {
+public:
+    yolov4(AAssetManager *mgr, const char *param, const char *bin, bool useGPU);
+
+    ~yolov4();
+
+    std::vector<BoxInfo> detect(JNIEnv *env, jobject image, float threshold, float nms_threshold);
+
+    std::vector<std::string> labels{"bus prority lane",
+    "children crossing",
+    "hospital",
+    "level crossing with gates",
+    "no honking",
+    "no left turn",
+    "no right turn",
+    "no u turn",
+    "pedestrian crossing",
+    "pedestrian crossing ahead",
+    "speed 40",
+    "speed 50",
+    "speed 60",
+    "speed 70",
+    "stop priority"};
+private:
+    static std::vector<BoxInfo> decode_infer(ncnn::Mat &data, const cv::Size &frame_size, float threshold);
+
+    ncnn::Net *Net;
+    int input_size = 416;
+
+    static void nms(std::vector<BoxInfo> &input_boxes, float NMS_THRESH);
+
+public:
+    static yolov4 *detector;
+    static bool hasGPU;
+    static bool toUseGPU;
+
+
+};
+
+
+#endif
